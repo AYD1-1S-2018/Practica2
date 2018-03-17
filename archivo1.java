cc250d062daa2287b39a45e94a3d0e02e2a21c5d
henrykre <henrykre@hotmail.es>
2018-03-17 00:44:33 -0600

    Se agregó la función: raíz cuadrada [E2]

--- archivo1.java
+++ archivo1.java
@@ -35,5 +35,11 @@ class Archivo1{
 		String resultado = Integer.toString(c1, 2);
 		return resultado;
 	}
+	public static String Raiz_Cuadrada(String a){
+		int a1 = Integer.parseInt(a, 2);
+		float c1 = (float)Math.sqrt(a1);
+		String resultado = Float.toString(c1);
+		return resultado;
+	}
 }
 
